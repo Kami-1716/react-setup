f4062fb57ba6392be4a8329907830b42a31d0d5c
kami-1716 <msf2100592@ue.edu.pk>
2023-12-03 02:48:34 -0800

    added functionality with react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-
-
-import Chai from "./chai"
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const username = "chai aur code"
+  const [counter, setCounter] = useState(0);
 
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter((counter) => counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter((counter) => counter - 1);
+    }
+  };
+
+  const resetValue = () => {
+    setCounter(0);
+  };
   return (
     <>
-    <h1>Hello React</h1>
-   </>
-  )
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+        style={{ backgroundColor: "black", color: "white", marginRight: 10 }}
+      >
+        Increment
+      </button>
+
+      <button
+        onClick={removeValue}
+        style={{ backgroundColor: "black", color: "white" }}
+      >
+        Decrement
+      </button>
+      <br />
+      <hr />
+      <button
+        onClick={resetValue}
+        style={{ backgroundColor: "red", color: "white" }}
+      >
+        Reset Counter
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
